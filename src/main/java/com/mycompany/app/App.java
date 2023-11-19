f859bcb6e7080763d21c07fb3caafe5b483f7f12
xuchi6 <xuchi6@jd.com>
2023-11-19 17:47:32 +0800

    Add thread sleep

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,8 @@
 package com.mycompany.app;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Hello world!
  */
@@ -7,10 +10,13 @@ public class App {
 
     private static final String MESSAGE = "Hello World!";
 
-    public App() {}
+    public App() {
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, UnknownHostException {
         System.out.println(MESSAGE);
+        Thread.sleep(60 * 1000L);
+        System.out.println(MESSAGE + " end" + "------>" + InetAddress.getLocalHost().getHostAddress());
     }
 
     public String getMessage() {
